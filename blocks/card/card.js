8b9163cd9618bb6c5cc3459997a42918debe90e8
Aravindo Wingeier <wingeier@adobe.com>
2023-07-31 17:07:39 -0700

    link image

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -1,13 +1,28 @@
-import {createOptimizedPicture, toClassName} from '../../scripts/lib-franklin.js';
+import {toClassName} from '../../scripts/lib-franklin.js';
 
+/**
+ * parameters: any of 'focus','fitness', 'fuel', 'recover', or 'large'
+ * content structure: in first cell:
+ *  - image
+ *  - CATEGORY (optional)
+ *  - title as linked h1, h2, h3, h4, h5, or h6
+ *  - author with link
+ */
 export default function decorate(block) {
   let firstCell = block.firstElementChild.firstElementChild;
-  firstCell.querySelector('picture').parentElement.classList.add('card-image');
   firstCell.querySelector('h1, h2, h3, h4, h5, h6').classList.add('card-title');
+
+  // link the image
+  const pictureParagraph = firstCell.querySelector('picture').parentElement;
+  const link = document.createElement('a');
+  link.href = block.querySelector('.card-title a').href;
+  link.append(...pictureParagraph.childNodes);
+  link.classList.add('card-image');
+  pictureParagraph.replaceWith(link);
+
   firstCell.lastElementChild.classList.add('card-author');
 
   // often there is a category like SUCCESS STORIES or GET STARTED.
-  // get first p without any class
   let category = firstCell.querySelector('p:not([class])');
   if(category){
     const link = document.createElement('a');
