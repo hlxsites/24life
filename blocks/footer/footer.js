43b514a8201bad10897e4fda9b913c5f558d25b3
Amit Shrigondekar <ashrigondeka@adobe.com>
2023-08-04 09:51:42 -0600

    footer ui updated

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,6 +1,21 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
 /**
+ * Creates an anchor element with the given image as its child
+ * @param img
+ * @param {string} link
+ * @returns {HTMLAnchorElement} anchor
+ */
+
+function createImageLink(img, link) {
+  const anchor = document.createElement('a');
+  anchor.href = link;
+  anchor.target = '_blank';
+  anchor.appendChild(img);
+  return anchor;
+}
+
+/**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
@@ -22,12 +37,60 @@ export default async function decorate(block) {
 
     const slogan = footerContainer.querySelector('div:nth-child(2) > p:nth-child(1)');
     slogan.classList.add('footer-slogan');
+
     footerContainer.querySelectorAll('ul')
-      .forEach((list) => {
-        list.classList.add('footer-menu-list');
+      .forEach((list, index) => {
+        list.classList.add(`footer-menu-list-${index}`);
       });
+
+    // add horizontal line
+    const list = footerContainer.querySelector('ul');
+    const horizontalLine = document.createElement('hr');
+    list.after(horizontalLine);
+
     const rightSide = footerContainer.querySelector('div:nth-child(3)');
-    rightSide.classList.add('rightSide');
+    rightSide.classList.add('right-side');
+
+    // Create link for powered by fitness logo
+    const poweredByFitnessImg = rightSide.querySelector('p:nth-last-child(2)');
+    const poweredByFitnessImgLink = createImageLink(poweredByFitnessImg, 'https://www.24hourfitness.com/');
+
+    // Create link for try workout logo
+    const tryWorkoutImg = rightSide.querySelector('p:last-child');
+    const tryWorkoutImgLink = createImageLink(tryWorkoutImg, 'https://www.24hourfitness.com/programs/24go/');
+
+    rightSide.append(poweredByFitnessImgLink);
+    rightSide.append(tryWorkoutImgLink);
+
+    const leftSide = footerContainer.querySelector('div:first-child');
+    leftSide.classList.add('left-side');
+
+    // Create link for Brand logo
+    const brandImg = leftSide.querySelector('picture');
+    const brandImgLink = createImageLink(brandImg, '/');
+    leftSide.append(brandImgLink);
+
+    const middleDiv = footerContainer.querySelector('div:nth-child(2)');
+    middleDiv.classList.add('middle-div');
+
+    // Trademark
+    const trademarkContainer = document.createElement('div');
+    trademarkContainer.classList.add('trademark-container');
+    const trademark = middleDiv.querySelector('p:last-child');
+    trademark.classList.add('trade-mark');
+    trademarkContainer.append(trademark);
+    middleDiv.append(trademarkContainer);
+
+    // Top button
+    const topBtnContainer = document.createElement('div');
+    topBtnContainer.classList.add('top-btn-container');
+    const topBtn = document.createElement('a');
+    topBtn.href = '#top';
+    topBtn.className = 'top-btn';
+    topBtn.textContent = 'Top';
+    topBtnContainer.append(topBtn);
+    middleDiv.appendChild(topBtnContainer);
+
     decorateIcons(footerContainer);
     block.append(footerContainer);
   }
