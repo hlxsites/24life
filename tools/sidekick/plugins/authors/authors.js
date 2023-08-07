e8b5359c1c0bbb5d7f9d04c967b5bdc84c7a64ee
Aravindo Wingeier <wingeier@adobe.com>
2023-08-07 15:45:45 -0700

    Show authors

--- tools/sidekick/plugins/authors/authors.js
+++ tools/sidekick/plugins/authors/authors.js
@@ -1,81 +1,49 @@
 import { PLUGIN_EVENTS } from 'https://www.hlx.live/tools/sidekick/library/events/events.js';
 // import { createOptimizedPicture, getMetadata } from '../../../../scripts/lib-franklin.js';
 
-const selectedTags = [];
+const selectedAuthors = [];
 
 export async function decorate(container, ignored, query) {
   const data = await getAuthorData();
 
   const createMenuItems = () => {
-    const filteredTags = getFilteredTags(data, query);
-    return filteredTags.map((item) => {
-      const isSelected = selectedTags.includes(item.tag);
+    const filteredAuthors = getFilteredAuthors(data, query);
+    return filteredAuthors.map((item) => {
+      const isSelected = selectedAuthors.includes(item.author);
       return `
-        <sp-menu-item value="${item.name}" ${isSelected ? 'selected' : ''}>
-          ${item.name}
+        <sp-menu-item value="${item.name} (${item['author-id']})" ${isSelected ? 'selected' : ''}>
+          ${item.name} (${item['author-id']})
         </sp-menu-item>
       `;
     }).join('');
   };
 
-  const handleMenuItemClick = (e) => {
-    const { value, selected } = e.target;
-    if (selected) {
-      const index = selectedTags.indexOf(value);
-      if (index > -1) {
-        selectedTags.splice(index, 1);
-      }
-    } else {
-      selectedTags.push(value);
-    }
-
-    const selectedLabel = container.querySelector('.selectedLabel');
-    selectedLabel.textContent = getSelectedLabel();
-  };
-
-  const handleCopyButtonClick = () => {
-    navigator.clipboard.writeText(selectedTags.join(', '));
+  const handleCopyButtonClick = (e) => {
+    const { value } = e.target;
+    navigator.clipboard.writeText(value);
     container.dispatchEvent(
       new CustomEvent(PLUGIN_EVENTS.TOAST, {
-        detail: { message: 'Copied Tags' },
+        detail: { message: 'Copied Author' },
       }),
     );
   };
 
-  const menuItems = createMenuItems();
-  const sp = /* html */`
-    <sp-menu
-      label="Select tags"
-      selects="multiple"
-      data-testid="taxonomy"
-    >
-      ${menuItems}
-    </sp-menu>
-    <sp-divider size="s"></sp-divider>
-    <div class="footer">
-      <span class="selectedLabel">${getSelectedLabel()}</span>
-      <sp-action-button label="Copy" quiet>
-        <sp-icon-copy slot="icon"></sp-icon-copy>
-      </sp-action-button>
-    </div>
-  `;
-
   const spContainer = document.createElement('div');
   spContainer.classList.add('container');
-  spContainer.innerHTML = sp;
+  spContainer.innerHTML = `
+    <sp-menu >
+      ${(createMenuItems())}
+    </sp-menu> `;
   container.append(spContainer);
 
   const menuItemElements = spContainer.querySelectorAll('sp-menu-item');
   menuItemElements.forEach((item) => {
-    item.addEventListener('click', handleMenuItemClick);
+    item.addEventListener('click', handleCopyButtonClick);
   });
-
-  const copyButton = spContainer.querySelector('sp-action-button');
-  copyButton.addEventListener('click', handleCopyButtonClick);
 }
 
 async function getAuthorData() {
-  const resp = await ffetch('/authors.json?');
+  const resp = await fetch('/authors.json?limit=5000');
   if (resp.ok) {
     const json = await resp.json();
     return json.data;
@@ -83,19 +51,16 @@ async function getAuthorData() {
   throw new Error('Error fetching authors.json');
 }
 
-function getSelectedLabel() {
-  return selectedTags.length > 0 ? `${selectedTags.length} tag${selectedTags.length > 1 ? 's' : ''} selected` : 'No tags selected';
-}
-
-function getFilteredTags(data, query) {
+function getFilteredAuthors(data, query) {
   if (!query) {
     return data;
   }
 
-  return data.filter((item) => item.tag.toLowerCase().includes(query.toLowerCase()));
+  return data.filter((item) => item.name.toLowerCase().includes(query.trim().toLowerCase())
+      || item['author-id'].toLowerCase().includes(query.trim().toLowerCase()));
 }
 
 export default {
-  title: 'Tags',
+  title: 'Authors',
   searchEnabled: true,
 };
