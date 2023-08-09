1f1b2d8fcf8cac330eb3a87edf9f86cf71861492
Aravindo Wingeier <wingeier@adobe.com>
2023-08-08 23:35:38 -0700

    Simplify

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -3,23 +3,22 @@ export default function decorate(block) {
     child.classList.add('slide');
   });
 
-  const nextButton = document.createElement('button');
-  nextButton.classList.add('next');
-  nextButton.textContent = 'Next';
-  const prevButton = document.createElement('button');
-  prevButton.classList.add('prev');
-  prevButton.textContent = 'Prev';
-
   let position = 0;
-  // block.style.transform = 'translateX(0)';
   const slideCount = block.querySelectorAll('.slide').length;
 
   function moveSlides(diff) {
     position = (slideCount + position + diff) % slideCount;
     block.style.setProperty('--position', position);
   }
+
+  // add buttons
+  const nextButton = document.createElement('button');
+  nextButton.classList.add('next');
+  nextButton.textContent = 'Next';
+  const prevButton = document.createElement('button');
+  prevButton.classList.add('prev');
+  prevButton.textContent = 'Prev';
   nextButton.addEventListener('click', () => moveSlides(+1));
   prevButton.addEventListener('click', () => moveSlides(-1));
-  moveSlides(0);
   block.append(prevButton, nextButton);
 }
