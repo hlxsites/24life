17e1b196629bd29a54a518ebbd93558e9ce35e50
Aravindo Wingeier <wingeier@adobe.com>
2023-08-09 00:27:32 -0700

    buttons method

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -5,20 +5,24 @@ export default function decorate(block) {
 
   let position = 0;
   const slideCount = block.querySelectorAll('.slide').length;
-
   function moveSlides(diff) {
     position = (slideCount + position + diff) % slideCount;
     block.style.setProperty('--position', position);
   }
 
-  // add buttons
-  const nextButton = document.createElement('button');
-  nextButton.classList.add('next');
-  nextButton.textContent = 'Next';
+  block.append(...createButtons(moveSlides));
+}
+
+function createButtons(moveSlides) {
   const prevButton = document.createElement('button');
   prevButton.classList.add('prev');
   prevButton.textContent = 'Prev';
-  nextButton.addEventListener('click', () => moveSlides(+1));
   prevButton.addEventListener('click', () => moveSlides(-1));
-  block.append(prevButton, nextButton);
+
+  const nextButton = document.createElement('button');
+  nextButton.classList.add('next');
+  nextButton.textContent = 'Next';
+  nextButton.addEventListener('click', () => moveSlides(+1));
+
+  return [prevButton, nextButton];
 }
