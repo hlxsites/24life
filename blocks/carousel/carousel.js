08e1dc0d171d3899be46883c814cd4917d7a1a09
Aravindo Wingeier <wingeier@adobe.com>
2023-08-09 12:58:52 -0700

    mostly works

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,16 +1,43 @@
 export default function decorate(block) {
-  [...block.children].forEach((child) => {
-    child.classList.add('slide');
+  const slideCount = block.childNodeCount;
+  [...block.children].forEach((child) => child.classList.add('slide'));
+
+  // make a total of 3 copies of the slides, so it appears to be infinite scrolling
+  block.append(...[...block.children].map((child) => child.cloneNode(true)));
+  block.append(...[...block.children].map((child) => child.cloneNode(true)));
+
+  function moveSlides(diff, smooth = 'smooth') {
+    // once the scroll is finished, make sure we are in the middle section
+    onScrollEnd(block, () => {
+      maybeJumpToCenterSlides(slideCount, block);
+    }, true);
+
+    const position = getCurrentPosition(block) + diff;
+    block.scrollTo({ top: 0, left: position * block.clientWidth, behavior: smooth });
+  }
+  block.addEventListener('scrollend', () => {
+    maybeJumpToCenterSlides(slideCount, block);
   });
 
-  let position = 0;
-  const slideCount = block.querySelectorAll('.slide').length;
-  function moveSlides(diff) {
-    position = (slideCount + position + diff) % slideCount;
-    block.style.setProperty('--position', position);
-  }
+  // set initial position
+  requestAnimationFrame(() => {
+    moveSlides(slideCount, 'instant');
+  });
 
-  block.append(...createButtons(moveSlides));
+  block.parentElement.append(...createButtons(moveSlides));
+}
+
+function getCurrentPosition(block) {
+  return block.scrollLeft / block.clientWidth;
+}
+
+function maybeJumpToCenterSlides(slideCount, block) {
+  // make sure we are in the middle section after each scroll
+  let position = getCurrentPosition(block);
+  if (position < slideCount || position >= slideCount * 2) {
+    position = slideCount + (position % slideCount);
+    block.scrollTo({ top: 0, left: position * block.clientWidth, behavior: 'instant' });
+  }
 }
 
 function createButtons(moveSlides) {
@@ -25,4 +52,23 @@ function createButtons(moveSlides) {
   nextButton.addEventListener('click', () => moveSlides(+1));
 
   return [prevButton, nextButton];
+}
+
+/** Fallback for Safari, see explanation on https://developer.chrome.com/blog/scrollend-a-new-javascript-event/
+ and https://stackoverflow.com/a/4620986/79461 */
+function onScrollEnd(block, callback, once = false) {
+  if ('onscrollend' in window) {
+    block.addEventListener('scrollend', callback, { once });
+  } else {
+    let timer = null;
+    const scrollListener = () => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        // no scrolling happened for 150ms
+        block.removeEventListener('scroll', scrollListener);
+        callback();
+      }, 150);
+    };
+    block.addEventListener('scroll', scrollListener);
+  }
 }
