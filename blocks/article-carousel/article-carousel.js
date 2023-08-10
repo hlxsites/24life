2246e773205fc5203fd8fabcf86e8df357d11ea1
Aravindo Wingeier <wingeier@adobe.com>
2023-08-09 20:30:52 -0700

    cleanup

--- blocks/article-carousel/article-carousel.js
+++ blocks/article-carousel/article-carousel.js
@@ -1,14 +1,16 @@
 import {
-  buildBlock, decorateBlock, loadBlock, loadBlocks, readBlockConfig, toClassName,
+  buildBlock, decorateBlock, loadBlock, readBlockConfig, toClassName,
 } from '../../scripts/lib-franklin.js';
 import ffetch from '../../scripts/ffetch.js';
 import { createCardBlock } from '../card/card.js';
 
-function createCarouselBlock(content) {
+async function createCarouselBlock(content) {
   const wrapper = document.createElement('div');
   const newBlock = buildBlock('carousel', content);
   wrapper.append(newBlock);
   decorateBlock(newBlock);
+  await loadBlock(wrapper.firstElementChild);
+
   return wrapper;
 }
 
@@ -16,8 +18,8 @@ export default async function decorate(block) {
   const { section } = readBlockConfig(block);
   block.textContent = '';
   block.classList.add('carousel-container');
-  // eslint-disable-next-line no-console
   const cards = await fetchArticlesAndCreateCards(section);
+
   // TODO: clean up this mess
   const groups = [
     [document.createDocumentFragment('div')],
@@ -29,18 +31,8 @@ export default async function decorate(block) {
     groups[Math.floor(index / 3)][0].append(card);
   });
   // TODO: also support groups of 2 and 1?
-  const carousel = createCarouselBlock(groups);
-  block.append(carousel);
 
-  // }
-  await loadBlock(carousel.firstElementChild);
-
-  // const footerBlock = buildBlock('footer', '');
-  // footer.append(footerBlock);
-  // decorateBlock(footerBlock);
-  // return loadBlock(footerBlock);
-
-  // await loadBlocks(block.closest('main'));
+  block.append(await createCarouselBlock(groups));
 }
 
 async function fetchArticlesAndCreateCards(filterSection) {
