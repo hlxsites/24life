58e0b8d489d16df245a7b7f62efaf70bfcb16154
Aravindo Wingeier <wingeier@adobe.com>
2023-08-10 13:19:23 -0700

    rework article-list filtering and card layout

--- blocks/article-list/article-list.js
+++ blocks/article-list/article-list.js
@@ -5,22 +5,25 @@ import ffetch from '../../scripts/ffetch.js';
 import { createCardBlock } from '../card/card.js';
 
 export default function decorate(block) {
-  let { by } = readBlockConfig(block);
-  if (!by && document.location.pathname.startsWith('/author/')) {
+  const filters = readBlockConfig(block);
+  if (!Object.keys(filters) && document.location.pathname.startsWith('/author/')) {
     // auto-detect author, e.g. https://www.24life.com/author/24life
-    by = new URL(document.location).pathname.split('/').pop();
+    filters.author = new URL(document.location).pathname.split('/').pop();
   }
   block.textContent = '';
   block.classList.add('card-container');
   // eslint-disable-next-line no-console
-  fetchArticlesAndAddCards(by, block).catch((e) => console.log(e));
+  fetchArticlesAndAddCards(filters, block).catch((e) => console.log(e));
 }
 
-async function fetchArticlesAndAddCards(by, block) {
+async function fetchArticlesAndAddCards(filters, block) {
   const articles = await ffetch('/articles.json').all();
 
   articles
-    .filter((article) => (by ? article['author-id'] === by : true))
+    // make sure all filters match
+    .filter((article) => Object.keys(filters).every(
+      (key) => article[key]?.toLowerCase() === filters[key].toLowerCase(),
+    ))
     .filter(({ template }) => template === 'article')
     .forEach((article) => {
       const newBlock = createCardBlock(article, block);
