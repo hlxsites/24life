67fe3e104a876a953a732f6d685a4d6838d0fe82
Aravindo Wingeier <wingeier@adobe.com>
2023-08-10 16:24:16 -0700

    improve filter

--- blocks/article-carousel/article-carousel.js
+++ blocks/article-carousel/article-carousel.js
@@ -4,45 +4,23 @@ import {
 import ffetch from '../../scripts/ffetch.js';
 import { createCardBlock } from '../card/card.js';
 
-async function createCarouselBlock(content) {
-  const wrapper = document.createElement('div');
-  const newBlock = buildBlock('carousel', '');
-  wrapper.append(newBlock);
-  newBlock.textContent = '';
-  newBlock.append(...content);
-  decorateBlock(newBlock);
-  await loadBlock(newBlock);
-
-  return wrapper;
-}
-
 export default async function decorate(block) {
-  const { section } = readBlockConfig(block);
+  const filters = readBlockConfig(block);
   block.textContent = '';
   block.classList.add('carousel-container');
-  const cards = await fetchArticlesAndCreateCards(section);
-
-  // TODO: clean up this mess
-  // const groups = [
-  //   document.createDocumentFragment('div'),
-  //   document.createDocumentFragment('div'),
-  //   document.createDocumentFragment('div'),
-  // ];
-  // // put cards in group of 3
-  // cards.forEach((card, index) => {
-  //   groups[Math.floor(index / 3)][0].append(card);
-  // });
-  // TODO: also support groups of 2 and 1?
-
+  const cards = await fetchArticlesAndCreateCards(filters);
   block.append(await createCarouselBlock(cards));
 }
 
-async function fetchArticlesAndCreateCards(filterSection) {
+async function fetchArticlesAndCreateCards(filters) {
   const dummyParent = document.createElement('div');
   return ffetch('/articles.json')
     .chunks(20)
     .filter(({ template }) => template === 'article')
-    .filter(({ section }) => (filterSection ? section === filterSection : true))
+  // make sure all filters match
+    .filter((article) => Object.keys(filters).every(
+      (key) => article[key]?.toLowerCase() === filters[key].toLowerCase(),
+    ))
     .limit(9)
     .map(async (article) => {
       const card = createCardBlock(article, dummyParent);
@@ -54,4 +32,16 @@ async function fetchArticlesAndCreateCards(filterSection) {
       return card;
     })
     .all();
+}
+
+async function createCarouselBlock(content) {
+  const wrapper = document.createElement('div');
+  const newBlock = buildBlock('carousel', '');
+  wrapper.append(newBlock);
+  newBlock.textContent = '';
+  newBlock.append(...content);
+  decorateBlock(newBlock);
+  await loadBlock(newBlock);
+
+  return wrapper;
 }
