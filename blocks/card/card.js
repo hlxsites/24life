31345c37fec68247716fa660994df8d0a288a97d
Amit Shrigondekar <ashrigondeka@adobe.com>
2023-08-10 11:28:01 -0600

    resetting card.js

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -1,5 +1,5 @@
 import {
-  buildBlock, buildAuthorCardBlock, createOptimizedPicture, decorateBlock, toClassName,
+  buildBlock, createOptimizedPicture, decorateBlock, toClassName,
 } from '../../scripts/lib-franklin.js';
 
 /**
@@ -11,9 +11,6 @@ import {
  *  - p > author with link (optional)
  */
 export default function decorate(block) {
-  // if block has author class then set a boolean
-  const isAuthor = block.classList.contains('author');
-
   const firstCell = block.firstElementChild.firstElementChild;
   firstCell.querySelector('h1, h2, h3, h4, h5, h6').classList.add('card-title');
 
@@ -42,22 +39,20 @@ export default function decorate(block) {
     firstCell.lastElementChild.classList.add('card-author');
   }
 
-  if (!isAuthor) {
-    // often there is a collection like SUCCESS STORIES or GET STARTED. Multiple categories
-    // can be comma separated.
-    const collections = firstCell.querySelector('p:not([class])');
-    if (collections) {
-      collections.classList.add('card-collections');
-      const links = collections.textContent.split(',')
-        .map((collectionText) => {
-          const a = document.createElement('a');
-          a.href = `/collections/${toClassName(collectionText.trim())}`;
-          a.append(collectionText.trim());
-          return a;
-        });
-      collections.innerHTML = '';
-      collections.append(...links);
-    }
+  // often there is a collection like SUCCESS STORIES or GET STARTED. Multiple categories
+  // can be comma separated.
+  const collections = firstCell.querySelector('p:not([class])');
+  if (collections) {
+    collections.classList.add('card-collections');
+    const links = collections.textContent.split(',')
+      .map((collectionText) => {
+        const a = document.createElement('a');
+        a.href = `/collections/${toClassName(collectionText.trim())}`;
+        a.append(collectionText.trim());
+        return a;
+      });
+    collections.innerHTML = '';
+    collections.append(...links);
   }
 
   const accentColor = ['focus', 'fitness', 'fuel', 'recover']
@@ -77,83 +72,17 @@ export default function decorate(block) {
   }
 }
 
-function p(content) {
-  const result = document.createElement('p');
-  result.append(content);
-  return result;
-}
-
-/* convenience function to create a block from a JSON object from authors.json */
-export function createAuthorCardBlock(author) {
-  const picture = createOptimizedPicture(author.image);
-
-  const heading = document.createElement('h3');
-  const anchor = document.createElement('a');
-  anchor.href = author.path;
-  anchor.textContent = author.name;
-  heading.append(anchor);
-
-  const authorLinkContainer = document.createElement('div');
-  authorLinkContainer.classList.add('author-links');
-  // parse the author.links string and iterate over links
-  addAuthorLinks(author, authorLinkContainer);
-
-  const newBlock = buildAuthorCardBlock('card', [
-    p(picture),
-    heading,
-    p(author.description),
-    authorLinkContainer,
-  ]);
-
-  const wrapper = document.createElement('div');
-  wrapper.append(newBlock);
-  newBlock.classList.add('author');
-  decorateBlock(newBlock);
-  return wrapper;
-}
-
-function addAuthorLinks(author, authorLinkContainer) {
-  if (!author.links) return;
-  let arr = JSON.parse(author.links);
-  if (arr.length > 0) {
-    arr = arr[0].split(',');
-  }
-  const socialLinksContainer = document.createElement('div');
-  socialLinksContainer.classList.add('social-links');
-  function addSocialLink(socialLink) {
-    const container = document.createElement('p');
-    container.appendChild(socialLink);
-    socialLinksContainer.appendChild(container);
-  }
-  arr.forEach((link) => {
-    const socialLink = document.createElement('a');
-    socialLink.href = link;
-    socialLink.target = '_blank';
-    if (link.includes('facebook')) {
-      socialLink.innerHTML = '<span class="icon icon-facebook"><svg xmlns="http://www.w3.org/2000/svg"><use href="#icons-sprite-facebook"></use></svg></span>';
-      addSocialLink(socialLink);
-    } else if (link.includes('twitter')) {
-      socialLink.innerHTML = '<span class="icon icon-twitter"><svg xmlns="http://www.w3.org/2000/svg"><use href="#icons-sprite-twitter"></use></svg></span>';
-      addSocialLink(socialLink);
-    } else if (link.includes('instagram')) {
-      socialLink.innerHTML = '<span class="icon icon-instagram"><svg xmlns="http://www.w3.org/2000/svg"><use href="#icons-sprite-instagram"></use></svg></span>';
-      addSocialLink(socialLink);
-    } else {
-      const pElement = document.createElement('p');
-      pElement.classList.add('other-links');
-      socialLink.innerHTML = link;
-      pElement.appendChild(socialLink);
-      authorLinkContainer.appendChild(pElement);
-    }
-  });
-  authorLinkContainer.prepend(socialLinksContainer);
-}
-
 /* convenience function to create a block from a JSON object from articles.json */
 export function createCardBlock(articleInfo) {
   const wrapper = document.createElement('div');
   const firstCell = document.createElement('div');
   firstCell.append(articleInfo.title);
+
+  function p(content) {
+    const result = document.createElement('p');
+    result.append(content);
+    return result;
+  }
 
   const picture = createOptimizedPicture(articleInfo.image);
 
