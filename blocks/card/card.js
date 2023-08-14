b21bc1d02356625fb24ba707a40ed67ec5da1ad4
Aravindo Wingeier <synox@users.noreply.github.com>
2023-08-14 12:47:16 -0700

    Feat #13 Carousel for articles (#71)
    * recursively load blocks before displaying them

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -73,8 +73,7 @@ export default function decorate(block) {
 }
 
 /* convenience function to create a block from a JSON object from articles.json */
-export function createCardBlock(articleInfo) {
-  const wrapper = document.createElement('div');
+export function createCardBlock(articleInfo, parent) {
   const firstCell = document.createElement('div');
   firstCell.append(articleInfo.title);
 
@@ -107,10 +106,10 @@ export function createCardBlock(articleInfo) {
       author],
   });
 
-  wrapper.append(newBlock);
+  parent.append(newBlock);
   decorateBlock(newBlock);
 
-  return wrapper;
+  return newBlock;
 }
 
 function replacePictureSizes(image, imageSizes) {
