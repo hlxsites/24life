e6c8fcf3a86553e0a6b44c298b38da827c5cc7b1
Amit Shrigondekar <ashrigondeka@adobe.com>
2023-08-16 09:33:28 -0600

    11-article-hero-block
    PR review suggestions

--- blocks/article-hero/article-hero.js
+++ blocks/article-hero/article-hero.js
@@ -31,49 +31,12 @@ export default function decorate(block) {
   h1.append(data.title);
   h1.classList.add('hero-title');
   overlay.append(h1);
+  overlay.append('By ');
 
   const authorLink = document.createElement('a');
   authorLink.href = `/author/${toClassName(data.author)}`;
   authorLink.textContent = data.author;
   overlay.append(authorLink);
-  window.addEventListener('resize', adjustHeroHeight);
-  window.addEventListener('load', adjustHeroHeight);
 
-  /**
-   * If the image height is smaller than the viewport height
-   * set the container height to the image height
-   */
-  function adjustHeroHeight() {
-    const image = document.querySelector('.article-hero img');
-    let articleHeroContainer = document.querySelector('div.article-hero.block');
-    if (image.height < window.innerHeight) {
-      articleHeroContainer.style.height = `${image.height}px`;
-    } else {
-      articleHeroContainer.style.height = '100vh';
-    }
-    // if the window width is less than 600px,
-    // add the hero-title as the first child of the next-sibling of article-hero-container
-    if (window.innerWidth < 600) {
-      articleHeroContainer = document.querySelector('div.article-hero-container');
-      const nextSibling = articleHeroContainer.nextElementSibling;
-      // query hero-overlay
-      const heroOverlay = document.querySelector('div.section.article-hero-container div.hero-overlay');
-      // create a copy of hero-overlay
-      const heroOverlayCopy = heroOverlay.cloneNode(true);
-      heroOverlayCopy.classList.add('hero-overlay-smaller-screen');
-      // prepend only if the nextSibling does not have a child with
-      // class hero-overlay-smaller-screen
-      if (!nextSibling.querySelector('.hero-overlay-smaller-screen')) {
-        nextSibling.prepend(heroOverlayCopy);
-      }
-    } else {
-      // if the window width is greater than 600px,
-      // remove the hero-overlay-smaller-screen
-      const heroOverlaySmallerScreen = document.querySelector('div.hero-overlay.hero-overlay-smaller-screen');
-      if (heroOverlaySmallerScreen) {
-        heroOverlaySmallerScreen.remove();
-      }
-    }
-  }
   block.append(overlay);
 }
