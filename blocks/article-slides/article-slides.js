39a88347659f200c543af2902f584fff68b55c93
Aravindo Wingeier <wingeier@adobe.com>
2023-08-18 12:05:59 -0700

    refactor

--- blocks/article-slides/article-slides.js
+++ blocks/article-slides/article-slides.js
@@ -1,19 +1,53 @@
-import {
-  createOptimizedPicture,
-  readBlockConfig,
-
-} from '../../scripts/lib-franklin.js';
+import { createOptimizedPicture, readBlockConfig } from '../../scripts/lib-franklin.js';
 import ffetch from '../../scripts/ffetch.js';
 
 /**
- * Generic carousel block, which can be used for any content or blocks.
- * Each row is a slide.
+ * Slideshow with recent articles. Supports swiping on touch screens.
  * @param block
  */
 export default async function decorate(block) {
   const config = readBlockConfig(block);
   block.textContent = '';
+  const { goToSlide } = setupSlideControls(block);
 
+  const slideshowButtons = document.createElement('div');
+  slideshowButtons.classList.add('slideshow-buttons');
+
+  const articles = await fetchArticles(config);
+  articles.forEach((article, index) => {
+    const slide = document.createElement('div');
+    slide.classList.add('slide');
+    slide.innerHTML = `
+      <div class="image">${createOptimizedPicture(article.image, article.title, index === 0).outerHTML}</div>
+      <div class="text">
+          <p class="subtitle">${plainText(article.section)}</p>
+          <p class="title">${plainText(article.title)}</p>
+          <p class="author">BY ${plainText(article.author)}</p>
+      </div> `;
+    block.append(slide);
+
+    const button = document.createElement('button');
+    button.ariaLabel = `go to slide ${article.title}`;
+    button.addEventListener('click', () => goToSlide(index));
+    slideshowButtons.append(button);
+
+    if (index === 0) {
+      slide.classList.add('active');
+      button.classList.add('active');
+    }
+  });
+
+  block.append(slideshowButtons);
+}
+
+async function fetchArticles(config) {
+  return ffetch('/articles.json')
+    .chunks(Number(config.limit || 9))
+    .limit(Number(config.limit || 9))
+    .all();
+}
+
+function setupSlideControls(block) {
   function goToSlide(index) {
     block.querySelector('.slide.active').classList.remove('active');
     [...block.querySelectorAll('.slide')].at(index).classList.add('active');
@@ -43,42 +77,6 @@ export default async function decorate(block) {
     goToSlide([...block.querySelectorAll('.slide')].indexOf(prevSlide));
   }
 
-  const articles = await fetchArticles(config);
-  const slides = articles.map((article, index) => {
-    const card = document.createElement('div');
-    card.classList.add('slide');
-    card.innerHTML = `
-    <div class="image">${createOptimizedPicture(article.image, article.title, index === 0).outerHTML}</div>
-    <div class="text">
-        <p class="subtitle">${plainText(article.section)}</p>
-        <p class="title">${plainText(article.title)}</p>
-        <p class="author">BY ${plainText(article.author)}</p>
-    </div> `;
-
-    if (index === 0) {
-      card.classList.add('active');
-    }
-    return card;
-  });
-
-  const slideshowButtons = document.createElement('div');
-  slideshowButtons.classList.add('slideshow-buttons');
-
-  articles.forEach((article, index) => {
-    const button = document.createElement('button');
-    button.ariaLabel = `go to Slide ${article.title}`;
-    if (index === 0) {
-      button.classList.add('active');
-    }
-    slideshowButtons.append(button);
-    button.addEventListener('click', () => goToSlide(index));
-    slideshowButtons.append(button);
-  });
-
-  block.append(...slides);
-  block.append(slideshowButtons);
-  autoplaySlides();
-
   /** detect swipe gestures on touch screens to advance slides */
   function gestureStart(event) {
     const touchStartX = event.changedTouches[0].screenX;
@@ -94,16 +92,14 @@ export default async function decorate(block) {
         // finger not moved enough, do nothing
       }
     }
+
     block.addEventListener('touchend', gestureEnd, { once: true });
   }
-  block.addEventListener('touchstart', gestureStart, false);
-}
 
-async function fetchArticles(config) {
-  return ffetch('/articles.json')
-    .chunks(Number(config.limit || 9))
-    .limit(Number(config.limit || 9))
-    .all();
+  block.addEventListener('touchstart', gestureStart, false);
+
+  autoplaySlides();
+  return { goToSlide };
 }
 
 /**
