041ae02997789a4d6005b44a8c5c00b31e9d83de
Amit Shrigondekar <ashrigondeka@adobe.com>
2023-08-21 17:33:44 -0600

    collection landing page

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -15,4 +15,22 @@ export default function decorate(block) {
       }
     });
   });
+
+  if (block.classList.contains('collections')) {
+    block.parentElement.classList.add('column-collections-parent');
+    // query anchor tags in the block
+    const anchors = [...block.querySelectorAll('a')];
+    anchors.forEach((anchor) => {
+      const newH1 = document.createElement('h2');
+      newH1.classList.add('columns-img-header');
+      newH1.innerHTML = anchor.textContent.toUpperCase();
+      anchor.text = '';
+      anchor.append(newH1);
+
+      const h1 = anchor.parentElement;
+      const p = h1.nextElementSibling;
+      anchor.append(p.querySelector('picture'));
+      p.remove();
+    });
+  }
 }
