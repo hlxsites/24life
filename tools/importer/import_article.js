84acfe1b141a93efe42d8eb091a34197830d5e54
Aravindo Wingeier <wingeier@adobe.com>
2023-08-22 13:12:46 -0700

    rearange methods

--- tools/importer/import_article.js
+++ tools/importer/import_article.js
@@ -1,6 +1,102 @@
 /* global WebImporter */
 /* eslint-disable no-console, class-methods-use-this, no-restricted-syntax, no-unused-vars */
 
+export default {
+  preprocess: ({
+    document, url, html, params,
+  }) => {
+    const ldJSON = document.querySelector('script[type="application/ld+json"]');
+    params.ldJSON = JSON.parse(ldJSON.textContent);
+  },
+
+  /**
+   * Apply DOM operations to the provided document and return
+   * the root element to be then transformed to Markdown.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @returns {HTMLElement} The root element to be transformed
+   */
+  transformDOM: ({
+    // eslint-disable-next-line no-unused-vars
+    document, url, html, params,
+  }) => {
+    // define the main element: the one that will be transformed to Markdown
+    const main = document.body;
+
+    // use helper method to remove header, footer, etc.
+    WebImporter.DOMUtils.remove(main, [
+      'header',
+      'footer',
+      'noscript',
+      '.nav-container',
+      '.breadcrumb',
+      '.tfl-related-posts-box-wrappper',
+      '.tfl-author-image',
+      '#disqus_thread',
+      'blockquote.wp-embedded-content',
+    ]);
+
+    // currently not supporting magazine articles, TODO: handle
+    if (document.querySelector('.mb_YTPlayer')) {
+      throw new Error('Magazine article not supported');
+    }
+    if (!document.querySelector('.tfl-page-title-wrap')) {
+      throw new Error('only normal articles are supported');
+    }
+
+    // start with h1, then image
+    const h1 = main.querySelector('h1');
+    main.prepend(h1);
+    const img = main.querySelector('img');
+    h1.after(img);
+
+    createMetadata(main, document, params);
+
+    // after getting the metadata, remove extra elements
+    WebImporter.DOMUtils.remove(main, [
+      '.page-title.image-bg',
+      '.tfl-the-tags',
+      '.post-title',
+      '.post-meta',
+      '.tags.pull-right',
+    ]);
+    for (const entry of main.querySelectorAll('.tfl-author-entry')) {
+      entry.closest('.row').remove();
+    }
+
+    removeLinksFromImagesPointingToItself(main);
+    moveFloatingImagesToNextLine(main, document);
+    makeCaptionTextItalics(main, document);
+    detectColumns(main, document);
+    detectYoutube(main, document);
+    articleEmbeds(main, document);
+    detectQuotes(main, document);
+    return main;
+  },
+
+  /**
+   * Return a path that describes the document being transformed (file name, nesting...).
+   * The path is then used to create the corresponding Word document.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @return {string} The path
+   */
+  generateDocumentPath: ({
+    document, url, html, params,
+  }) => {
+    const filename = WebImporter.FileUtils.sanitizePath(new URL(url).pathname.replace(/\.html$/, '').replace(/\/$/, ''));
+    const { category, year } = params;
+    if (!category || !year) {
+      throw new Error(`missing params categories or year. ${JSON.stringify(params)}`);
+    }
+    return `${toClassName(category)}/${toClassName(year)}/${filename}`;
+  },
+};
+
 export function toClassName(name) {
   return typeof name === 'string'
     ? name.toLowerCase().replace(/[^0-9a-z]/gi, '-').replace(/-+/g, '-').replace(/^-|-$/g, '')
@@ -179,99 +275,3 @@ function articleEmbeds(main, document) {
     }
   }
 }
-
-export default {
-  preprocess: ({
-    document, url, html, params,
-  }) => {
-    const ldJSON = document.querySelector('script[type="application/ld+json"]');
-    params.ldJSON = JSON.parse(ldJSON.textContent);
-  },
-
-  /**
-   * Apply DOM operations to the provided document and return
-   * the root element to be then transformed to Markdown.
-   * @param {HTMLDocument} document The document
-   * @param {string} url The url of the page imported
-   * @param {string} html The raw html (the document is cleaned up during preprocessing)
-   * @param {object} params Object containing some parameters given by the import process.
-   * @returns {HTMLElement} The root element to be transformed
-   */
-  transformDOM: ({
-    // eslint-disable-next-line no-unused-vars
-    document, url, html, params,
-  }) => {
-    // define the main element: the one that will be transformed to Markdown
-    const main = document.body;
-
-    // use helper method to remove header, footer, etc.
-    WebImporter.DOMUtils.remove(main, [
-      'header',
-      'footer',
-      'noscript',
-      '.nav-container',
-      '.breadcrumb',
-      '.tfl-related-posts-box-wrappper',
-      '.tfl-author-image',
-      '#disqus_thread',
-      'blockquote.wp-embedded-content',
-    ]);
-
-    // currently not supporting magazine articles, TODO: handle
-    if (document.querySelector('.mb_YTPlayer')) {
-      throw new Error('Magazine article not supported');
-    }
-    if (!document.querySelector('.tfl-page-title-wrap')) {
-      throw new Error('only normal articles are supported');
-    }
-
-    // start with h1, then image
-    const h1 = main.querySelector('h1');
-    main.prepend(h1);
-    const img = main.querySelector('img');
-    h1.after(img);
-
-    createMetadata(main, document, params);
-
-    // after getting the metadata, remove extra elements
-    WebImporter.DOMUtils.remove(main, [
-      '.page-title.image-bg',
-      '.tfl-the-tags',
-      '.post-title',
-      '.post-meta',
-      '.tags.pull-right',
-    ]);
-    for (const entry of main.querySelectorAll('.tfl-author-entry')) {
-      entry.closest('.row').remove();
-    }
-
-    removeLinksFromImagesPointingToItself(main);
-    moveFloatingImagesToNextLine(main, document);
-    makeCaptionTextItalics(main, document);
-    detectColumns(main, document);
-    detectYoutube(main, document);
-    articleEmbeds(main, document);
-    detectQuotes(main, document);
-    return main;
-  },
-
-  /**
-   * Return a path that describes the document being transformed (file name, nesting...).
-   * The path is then used to create the corresponding Word document.
-   * @param {HTMLDocument} document The document
-   * @param {string} url The url of the page imported
-   * @param {string} html The raw html (the document is cleaned up during preprocessing)
-   * @param {object} params Object containing some parameters given by the import process.
-   * @return {string} The path
-   */
-  generateDocumentPath: ({
-    document, url, html, params,
-  }) => {
-    const filename = WebImporter.FileUtils.sanitizePath(new URL(url).pathname.replace(/\.html$/, '').replace(/\/$/, ''));
-    const { category, year } = params;
-    if (!category || !year) {
-      throw new Error(`missing params categories or year. ${JSON.stringify(params)}`);
-    }
-    return `${toClassName(category)}/${toClassName(year)}/${filename}`;
-  },
-};
