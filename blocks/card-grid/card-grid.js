15bea679b84e8027829d35d185b219866d19001a
Rob Rusher <robrusher@gmail.com>
2023-08-24 15:58:28 -0600

    add comments

--- blocks/card-grid/card-grid.js
+++ blocks/card-grid/card-grid.js
@@ -4,6 +4,7 @@ import {
 import ffetch from '../../scripts/ffetch.js';
 import { createCardBlock } from '../card/card.js';
 
+// call articles once for block
 const articles = await ffetch('/articles.json').chunks(700).all();
 
 export default async function decorate(block) {
@@ -12,19 +13,20 @@ export default async function decorate(block) {
   links.forEach(async (link) => {
     const articleURL = new URL(link.innerHTML);
     await fetchArticleAndCreateCard(articleURL.pathname, link);
-    block.firstElementChild.remove();
+    block.firstElementChild.remove(); // remove the top node
     link.classList.remove('button', 'primary');
-    block.append(link);
+    block.append(link); // append new node to bottom
   });
 }
 
 async function fetchArticleAndCreateCard(path, link) {
   await Promise.all(articles
-    // make sure all filters match
+    // find the matching article via the path
     .filter((article) => article.path?.toLowerCase() === path.toLowerCase())
     .map(async (article) => {
       const wrapper = document.createElement('div');
       const card = createCardBlock(article, wrapper);
+      // set the section color
       if (article.section) {
         card.classList.add(toClassName(article.section));
       }
