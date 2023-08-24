0ef4eecc4efd479b7d2cbb94d371154457f7c9a2
Aravindo Wingeier <wingeier@adobe.com>
2023-08-24 16:52:46 -0700

    add validation tools

---
+++ tools/validation/24life-download-hlx.mjs
@@ -0,0 +1,70 @@
+/*  fetches existing franklin documents and stores them in a folder structure.
+    I use this to research existing blocks for certain content, or look for invalid patterns.
+ */
+
+import pLimit from 'p-limit';
+import fetch from 'node-fetch';
+import fs from "fs";
+import path from "path";
+
+const limitConcurrency = pLimit(5);
+const DOMAIN = "https://main--24life--hlxsites.hlx.page";
+
+async function main() {
+    const response = await fetch("https://main--24life--hlxsites.hlx.page/articles.json");
+    const json = await response.json();
+    const urls = json.data.map(row => DOMAIN + row.path)
+
+    await Promise.all(urls.map(async (url) => {
+        await(limitConcurrency(()=>loadUrl(url)));
+    }));
+}
+
+
+function getFolderPathAndFilename(url) {
+    const prefix = `./24life`;
+
+    const pathname = new URL(url).pathname;
+    // remove trailing slash
+    const withoutTrailingSlash = pathname.replace(/\/$/, '');
+
+    const folderName = withoutTrailingSlash.substring(0, withoutTrailingSlash.lastIndexOf('/'));
+    const folderPath = `${prefix}${folderName}`;
+
+    const filename = path.basename(withoutTrailingSlash.substring(0, 80)) + '.html';
+
+    return [folderPath, filename];
+}
+
+async function createFolder(folderPath) {
+    try {
+        await fs.promises.mkdir(folderPath, {recursive: true});
+    } catch (ignore) {
+        console.log('failed to create folder: ', folderPath);
+    }
+}
+
+
+async function loadUrl(url) {
+
+    try {
+        const [folderPath, filename] = getFolderPathAndFilename(url);
+        const response = await fetch(url, {redirect: 'error'});
+        if (response.ok) {
+
+            await createFolder(folderPath);
+            await response.body.pipe(fs.createWriteStream(`${folderPath}/${filename}`));
+            console.log('ok: ', url);
+        } else {
+            console.log('failed: ', url, response.status);
+        }
+    } catch (e) {
+        if (e.message.includes("uri requested responds with a redirect")) {
+            console.log('failed (redirect): ', url);
+            return;
+        }
+        console.log('failed: ', url, e.message);
+    }
+}
+
+main();
---
+++ tools/validation/24life-find-invalid-docs.mjs
@@ -0,0 +1,58 @@
+/*  looks for bad patterns in the final franklin output.
+ */
+
+import fs from 'fs/promises'
+import path from "path";
+
+async function main() {
+    // astrisk close together, see https://github.com/adobe/helix-importer/issues/217
+    await checkForString(new RegExp("\\*.{0,10}\\*"));
+
+    // comments
+    await checkForString("<!--");
+    await checkForString("-->");
+
+    // image links to the old site. videos need to be upload with slack bot, pdfs added to sharepoint.
+    await checkForString("twentyfourlife.wpenginepowered");
+
+    // vimeo links need to be removed if invalid. 
+    await checkForString("player.vimeo.com");
+}
+
+async function searchFilesInDirectoryAsync(dir, searchText) {
+    const files = await fs.readdir(dir);
+    const searchResults = await Promise.all(
+        files.map(async (file) => {
+            const filePath = path.join(dir, file);
+            const stats = await fs.stat(filePath);
+
+            if (stats.isDirectory()) {
+                return searchFilesInDirectoryAsync(filePath, searchText);
+            } else if (stats.isFile()) {
+                const content = await fs.readFile(filePath, 'utf-8');
+                return content.match(searchText) ? filePath : null;
+            }
+        })
+    );
+
+    return searchResults.flat().filter((result) => result !== null);
+}
+
+
+async function checkForString(searchText) {
+    const folderPath = `./24life`;
+
+    const searchResults = await searchFilesInDirectoryAsync(folderPath, searchText);
+    const withoutHtml = searchResults.map((result) => result.replace(".html", ""));
+    const filenameOnly = withoutHtml.map((result) => path.basename(result));
+    console.log();
+    console.log(`### ${searchText} (${filenameOnly.length})###`);
+    if (filenameOnly.length) {
+        console.log("preview:", withoutHtml.map((result) => `https://main--24life--hlxsites.hlx.page${result.substring(6)}`).join("\n"));
+        console.log()
+        console.log("reference:", filenameOnly.map((result) => `https://www.24life.com/${result}/`).join("\n"));
+        console.log()
+    }
+}
+
+main();
