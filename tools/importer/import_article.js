957f2071e3cfac92387080b510b74b1719794792
Aravindo Wingeier <wingeier@adobe.com>
2023-08-24 14:22:38 -0700

    import: remove exceptions

--- tools/importer/import_article.js
+++ tools/importer/import_article.js
@@ -40,9 +40,7 @@ export default {
       throw new Error('Magazine article not supported');
     }
     if (!document.querySelector('.tfl-page-title-wrap')) {
-      if (url !== 'https://www.24life.com/a-personal-trainer-on-metabolism-and-eating-more-to-lose-weight/') {
-        throw new Error('only normal articles are supported');
-      }
+      throw new Error('only normal articles are supported');
     }
 
     // start with h1, then image
