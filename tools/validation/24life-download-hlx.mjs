b737c386175b30c128cb2976f4463ef8158e0faa
Aravindo Wingeier <wingeier@adobe.com>
2023-08-24 17:46:44 -0700

    dont limit for downloads

--- tools/validation/24life-download-hlx.mjs
+++ tools/validation/24life-download-hlx.mjs
@@ -11,7 +11,7 @@ const limitConcurrency = pLimit(5);
 const DOMAIN = "https://main--24life--hlxsites.hlx.page";
 
 async function main() {
-    const response = await fetch("https://main--24life--hlxsites.hlx.page/articles.json");
+    const response = await fetch("https://main--24life--hlxsites.hlx.page/articles.json?limit=9999");
     const json = await response.json();
     const urls = json.data.map(row => DOMAIN + row.path)
 
