e9b3de1cca2bac84cb06dccb3f8dd753b1ad9493
Meet Kumar Jain <meetkjain83@gmail.com>
2023-08-29 19:02:32 +0530

    Coding the load more button implementation

--- blocks/article-list/article-list.js
+++ blocks/article-list/article-list.js
@@ -33,12 +33,15 @@ function removeEmptyKeyOrValue(obj) {
 
 async function fetchArticlesAndAddCards(filters, block) {
   const articles = await ffetch('/articles.json').all();
-
+  const numInitialLodedArticles = 30;
+  const actualLength = articles.filter((article) => Object.keys(filters).every(
+    (key) => article[key]?.toLowerCase().includes(filters[key].toLowerCase()),
+  )).length;
   await Promise.all(articles
     // make sure all filters match
     .filter((article) => Object.keys(filters).every(
       (key) => article[key]?.toLowerCase().includes(filters[key].toLowerCase()),
-    ))
+    )).slice(0, numInitialLodedArticles)
     .map(async (article) => {
       const wrapper = document.createElement('div');
       const newBlock = createCardBlock(article, wrapper);
@@ -48,4 +51,40 @@ async function fetchArticlesAndAddCards(filters, block) {
       block.append(wrapper);
       await loadBlock(newBlock);
     }));
+  let counter = 0;
+  if (actualLength > numInitialLodedArticles) {
+    const { loadMoreButton, loadMoreContainer } = createLoadMoreButton();
+    block.append(loadMoreContainer);
+    loadMoreButton.addEventListener('click', async () => {
+      counter += 1;
+      await Promise.all(articles
+        .filter((article) => Object.keys(filters).every(
+          (key) => article[key]?.toLowerCase().includes(filters[key].toLowerCase()),
+        )).slice(numInitialLodedArticles * counter, (numInitialLodedArticles * counter) + 30)
+        .map(async (article) => {
+          const wrapper = document.createElement('div');
+          const newBlock = createCardBlock(article, wrapper);
+          if (article.section) {
+            newBlock.classList.add(toClassName(article.section));
+          }
+          block.append(wrapper);
+          await loadBlock(newBlock);
+        }));
+      loadMoreContainer.remove();
+      if ((actualLength - (numInitialLodedArticles * counter)) > 30) {
+        const { loadMoreButton, loadMoreContainer } = createLoadMoreButton();
+        block.append(loadMoreContainer);
+      }
+    });
+  }
+}
+
+function createLoadMoreButton() {
+  const loadMoreContainer = document.createElement('div');
+  loadMoreContainer.classList.add('article-load-more-container');
+  const loadMoreButton = document.createElement('button');
+  loadMoreContainer.append(loadMoreButton);
+  loadMoreButton.classList.add('article-list-load-more-button');
+  loadMoreButton.textContent = 'Load more';
+  return { loadMoreButton, loadMoreContainer };
 }
