e653f2283683ed67ff09f59cc956298bd6288d71
Amit Shrigondekar <ashrigondeka@adobe.com>
2023-09-06 12:23:07 -0600

    single card added

--- tools/importer/import_magazine.js
+++ tools/importer/import_magazine.js
@@ -1,6 +1,48 @@
 /* global WebImporter */
 /* eslint-disable no-console, class-methods-use-this, no-restricted-syntax, no-unused-vars */
 
+function createQuoteBlock(main, document) {
+  const quote = document.querySelector('.wpb_text_column.wpb_content_element .wpb_wrapper p.text-style-title-2').textContent;
+  let author = document.querySelector('.wpb_text_column.wpb_content_element .wpb_wrapper p.text-style-body-2').textContent;
+  // remove leading non word characters so – Rhonda Byrne becomes Rhonda Byrne
+  author = author.replace(/^\W+/, '');
+
+  main.append(generateBlock(document, { Text: quote, Author: author }, 'Quote'));
+  main.innerHTML += '<p> --- </p>';
+}
+
+function createYoutubeLink(document, main) {
+  const youtubeLink = document.querySelector('.embed-video-container iframe.embed-responsive-item').src;
+  main.append(youtubeLink);
+  main.innerHTML += `${youtubeLink} <p> --- </p>`;
+}
+
+function createSingleCardBlock(main, document) {
+
+
+  const content = [];
+  const card = document.createElement('div'); 
+  card.append(document.querySelector('.vc_col-sm-12.vc_gitem-col.vc_gitem-col-align-.mindset img'));
+  
+  // Get Started
+  const getStarted = document.querySelector('.vc_col-sm-12.tfl-post-grid-col.vc_gitem-col.vc_gitem-col-align-.mindset .tfl-tags a:first-child').textContent.toUpperCase();
+  const successStories = document.querySelector('.vc_col-sm-12.tfl-post-grid-col.vc_gitem-col.vc_gitem-col-align-.mindset .tfl-tags a:last-child').textContent.toUpperCase();
+  card.append(document.createTextNode(`${getStarted}, ${successStories}`));
+
+  card.innerHTML += '<br/>';
+  
+  // article link
+  card.append(document.querySelector('.vc_custom_heading.tfl-post-grid-title.vc_gitem-post-data.vc_gitem-post-data-source-post_title a'));
+
+  card.innerHTML += '<br/>';
+
+  // author 
+  const author = document.querySelector('.vc_grid-container.vc_clearfix.wpb_content_element.vc_basic_grid .mindset .tfl-post-grid-author a').textContent;
+  card.append(document.createTextNode(`By: ${author}`));
+  content.push(card);
+  main.append(WebImporter.DOMUtils.createTable([['Card (Focus, Large)'], content], document));
+}
+
 export default {
   preprocess: ({
     document, url, html, params,
@@ -29,7 +71,19 @@ export default {
 
     createMetadata(main, document, params);
     console.log('metadata created');
-    createMagazineHero(main, document, params);
+
+    await createMagazineHero(main, document, params);
+    console.log('MagazineHero created');
+
+    createYoutubeLink(document, main);
+    console.log('youtubeLink created');
+
+    createQuoteBlock(main, document);
+    console.log('Quote created');
+
+    createSingleCardBlock(main, document);
+    console.log('SingleCard created');
+
 
     const filename = new URL(url).pathname
       .replace(/\/$/, '')
@@ -59,9 +113,8 @@ const createMetadata = (main, document, params) => {
   meta.Title = document.querySelector('title').textContent.replace(/-\s*24Life/gm, '');
   meta['Publication Date'] = ldJSON['@graph'].find((item) => item['@type'] === 'WebPage')?.datePublished.replace(/T.*$/, '');
   meta.Image = document.querySelector('body > div.main-container > section > div > div > div > div > div > div > section > div > div > div:nth-child(1) > div > div > div.wpb_single_image.wpb_content_element.vc_align_center > figure > a > img');
-  console.log('meta.Image:', meta.Image);
   const block = generateBlock(document, meta, 'Metadata');
-  main.prepend(block);
+  main.append(block);
   return meta;
 };
 
@@ -72,15 +125,15 @@ const createMagazineHero = async (main, document, params) => {
   magazineHero.Issue = layers[layers.length - 3];
   magazineHero.Title = layers[layers.length - 2].textContent;
   magazineHero.Description = layers[layers.length - 1].textContent;
-
+  await waitForElement('.tp-selecttoggle.rs-layer-static.rs-layer.rs-waction', document);
   const pillars = document.querySelectorAll('.tp-selecttoggle.rs-layer-static.rs-layer.rs-waction');
-  magazineHero.Focus = pillars[0].innerHTML
+  magazineHero.Focus = pillars[0].textContent
     + link(pillars[0].innerHTML, extractURL(pillars[0].outerHTML));
-  magazineHero.Fitness = pillars[1].innerHTML
+  magazineHero.Fitness = pillars[1].textContent
     + link(pillars[1].innerHTML, extractURL(pillars[1].outerHTML));
-  magazineHero.Fuel = pillars[2].innerHTML
+  magazineHero.Fuel = pillars[2].textContent
     + link(pillars[2].innerHTML, extractURL(pillars[2].outerHTML));
-  magazineHero.Recover = pillars[3].innerHTML
+  magazineHero.Recover = pillars[3].textContent
     + link(pillars[3].innerHTML, extractURL(pillars[3].outerHTML));
 
   magazineHero.Image = [];
@@ -90,7 +143,7 @@ const createMagazineHero = async (main, document, params) => {
     magazineHero.Image.push();
   });
 
-  main.prepend(generateBlock(document, magazineHero, 'magazine-hero'));
+  main.append(generateBlock(document, magazineHero, 'magazine-hero'));
 };
 
 function link(text, url) {
@@ -154,4 +207,19 @@ function generateBlock(document, metadata, blockName) {
     }
   }
   return table;
+}
+
+async function waitForElement(selector, document, timeout = 5000, interval = 250) {
+  return new Promise((resolve, reject) => {
+    const timeWas = new Date();
+    const wait = setInterval(() => {
+      if (document.querySelectorAll(selector)) {
+        clearInterval(wait);
+        resolve();
+      } else if (new Date() - timeWas > timeout) { // Timeout
+        clearInterval(wait);
+        reject();
+      }
+    }, interval);
+  });
 }
