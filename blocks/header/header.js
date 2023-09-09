ea8ceb681880192d55c350cf1a76d10e2850f060
Aravindo Wingeier <synox@users.noreply.github.com>
2023-09-08 18:50:09 -0700

    fix #130: make navigation fixed (#137)
    * simplify code
    * prevent concurrent fetching of menu content
    * make menu hoverable after changing from mobile to desktop view
    * lazy-load menu content

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,13 +1,10 @@
-import {
-  decorateIcons,
-  loadBlocks,
-} from '../../scripts/lib-franklin.js';
-import {
-  decorateMain,
-} from '../../scripts/scripts.js';
+import { decorateIcons, loadBlocks, toClassName } from '../../scripts/lib-franklin.js';
+import { decorateMain } from '../../scripts/scripts.js';
 
 // media query match that indicates mobile/desktop switch
 const MQ = window.matchMedia('(min-width: 992px)');
+
+// prevent concurrent creation of the div while the nav content is being loaded
 const CREATED = {
   focus: false, fitness: false, fuel: false, recover: false, magazine: false,
 };
@@ -17,14 +14,8 @@ function toggleMenu(header, sectionToOpen) {
   if (openSection === sectionToOpen) {
     return; // it's already open
   }
-  if (openSection) {
-    openSection.classList.remove('expand');
-    openSection.setAttribute('aria-expanded', 'false');
-  }
-  if (sectionToOpen) {
-    sectionToOpen.classList.add('expand');
-    sectionToOpen.setAttribute('aria-expanded', 'true');
-  }
+  openSection?.setAttribute('aria-expanded', 'false');
+  sectionToOpen?.setAttribute('aria-expanded', 'true');
 }
 
 function closeMenuSection(event) {
@@ -32,35 +23,43 @@ function closeMenuSection(event) {
   if (!navArea.contains(event.target)) {
     const openMenu = document.querySelector('header [aria-expanded="true"]');
     if (openMenu) {
-      openMenu.classList.remove('expand');
       openMenu.setAttribute('aria-expanded', 'false');
     }
   }
 }
 
 async function buildSectionMenuContent(header, section) {
-  const menu = await fetch(`/fragments/menu-${section}.plain.html`);
-  if (menu.ok && !CREATED[section]) {
-    CREATED[section] = true;
-    const fragment = document.createElement('div');
-    fragment.classList.add('nav-fragment', section);
-    fragment.innerHTML = await menu.text();
-    decorateMain(fragment);
-    await loadBlocks(fragment);
-    header.append(fragment);
-    toggleMenu(header, fragment);
+  if (CREATED[section]) return;
+
+  CREATED[section] = true;
+  try {
+    const menu = await fetch(`/fragments/menu-${section}.plain.html`);
+    if (menu.ok) {
+      const fragment = document.createElement('div');
+      fragment.classList.add('nav-fragment', section);
+      fragment.innerHTML = await menu.text();
+      fragment.setAttribute('aria-expanded', 'false');
+      decorateMain(fragment);
+      await loadBlocks(fragment);
+      header.append(fragment);
+    }
+  } catch (e) {
+    CREATED[section] = false;
+    throw e;
   }
 }
 
 // toggle from event
-function mouseOverMenu(header) {
-  const section = this.textContent.toLowerCase();
-  const sectionToOpen = header.querySelector(`:scope > .nav-fragment.${section}`);
-  if (sectionToOpen == null && !CREATED[section]) {
-    buildSectionMenuContent(header, section);
-  } else {
-    toggleMenu(header, sectionToOpen);
+async function mouseOverMenu(a, header) {
+  if (!MQ.matches) {
+    return; // only on desktop
   }
+  const section = a.textContent.toLowerCase();
+  if (!CREATED[section]) {
+    await buildSectionMenuContent(header, section);
+  }
+  const sectionToOpen = header.querySelector(`:scope > .nav-fragment.${section}`);
+  toggleMenu(header, sectionToOpen);
 }
 
 /**
@@ -127,27 +126,21 @@ export default async function decorate(block) {
     const sectionList = nav.querySelector('.sections .sections-list');
 
     // get through all section menus
-    const sectionMenus = [...navContent.children[2].querySelectorAll('ul > li')].map((menuBlock, index) => {
-      const navSection = document.createElement('li');
-      navSection.className = 'section';
-      if (index === 0) navSection.classList.add('color-focus');
-      else if (index === 1) navSection.classList.add('color-fitness');
-      else if (index === 2) navSection.classList.add('color-fuel');
-      else if (index === 3) navSection.classList.add('color-recover');
-      else if (index === 4) navSection.classList.add('color-magazine');
-      const sectionTitle = menuBlock.firstElementChild.textContent;
-      const a = document.createElement('a');
-      a.href = `/${sectionTitle.toLowerCase()}`;
-      a.textContent = sectionTitle;
-      const icon = document.createElement('span');
-      icon.classList.add('icon', 'icon-arrow-down');
-      if (MQ.matches) {
-        navSection.addEventListener('mouseover', mouseOverMenu.bind(a, block.parentNode));
-      }
-      navSection.append(a);
-      navSection.append(icon);
-      return navSection;
-    });
+    const sectionMenus = [...navContent.children[2].querySelectorAll('ul > li')]
+      .map((menuBlock) => {
+        const sectionTitle = menuBlock.firstElementChild.textContent;
+        const navSection = document.createElement('li');
+        navSection.classList.add('section', `color-${toClassName(sectionTitle)}`);
+        const a = document.createElement('a');
+        a.href = `/${sectionTitle.toLowerCase()}`;
+        a.textContent = sectionTitle;
+        const icon = document.createElement('span');
+        icon.classList.add('icon', 'icon-arrow-down');
+        navSection.addEventListener('mouseover', () => mouseOverMenu(a, block.parentNode));
+        navSection.append(a);
+        navSection.append(icon);
+        return navSection;
+      });
     // write the section menu
     if (sectionMenus.length) {
       sectionList.append(...sectionMenus);
@@ -187,7 +180,7 @@ export default async function decorate(block) {
     // force hamburger close when in destop size
     MQ.addEventListener('change', () => {
       document.querySelector('header nav .hamburger-toggle').setAttribute('aria-expanded', 'false');
-      document.querySelectorAll('header nav .sections .expand').forEach((el) => el.classList.remove('expand'));
+
       // remove hard styled heights (from animations)
       document.querySelectorAll('header nav .sections ul').forEach((ul) => {
         ul.style.height = null;
@@ -195,7 +188,15 @@ export default async function decorate(block) {
     });
 
     decorateIcons(nav);
-    /* append result */
     block.append(nav);
+  }
+
+  // load the content nav in the background with a small delay (only on desktop)
+  if (MQ.matches) {
+    setTimeout(() => {
+      for (const section of ['focus', 'fitness', 'fuel', 'recover', 'magazine']) {
+        buildSectionMenuContent(block.parentNode, section);
+      }
+    }, 500);
   }
 }
