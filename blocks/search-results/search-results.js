87ef95be4d9803131ec91869d7bbfdb989b55ebc
Meet Kumar Jain <meetkjain83@gmail.com>
2023-09-11 00:58:53 +0530

    Adding code for avoiding duplicate entries while search

--- blocks/search-results/search-results.js
+++ blocks/search-results/search-results.js
@@ -1,7 +1,11 @@
 import { createCardBlock } from '../card/card.js';
 import { loadBlock } from '../../scripts/lib-franklin.js';
 
+let newArray = [];
+let total = [];
+
 export async function searchResults(params) {
+  // fetch results from json files
   const allData = await fetch(`${window.location.origin}/articles.json?sheet=full`);
   console.log(allData);
   const jsonData = await allData.json();
@@ -18,13 +22,36 @@ export async function searchResults(params) {
     .includes(params.toLowerCase()));
 }
 
+function totalArray(arrayChunk) {
+  newArray = newArray.concat(arrayChunk);
+  return newArray;
+}
+
+function createCards(finalArray, block) {
+  finalArray.map(async (x) => {
+    const wrapper = document.createElement('div');
+    const newBlock = createCardBlock(x, wrapper);
+    block.append(wrapper);
+    await loadBlock(newBlock);
+  });
+}
+
+function createSet(sumArray, block) {
+  const uniquePath = new Set();
+  const finalArray = sumArray.filter((element) => {
+    const isDuplicate = uniquePath.has(element.path);
+    if (!isDuplicate) {
+      uniquePath.add(element.path);
+      return true;
+    }
+    return false;
+  });
+  console.log(finalArray);
+  createCards(finalArray, block);
+}
+
 export default async function decorate(block) {
-  // fetch results from json files
   block.innerHTML = '';
-  // const allData = await fetch(`${window.location.origin}/articles.json?sheet=full`);
-  // console.log(allData);
-  // const jsonData = await allData.json();
-  // for search functionality
   const searchTerm = new URLSearchParams(window.location.search).get('q');
   console.log(searchTerm);
   block.classList.add('card-container', 'three-columns');
@@ -33,17 +60,13 @@ export default async function decorate(block) {
     const inputArray = searchTerm.split(' ');
     if (inputArray.length > 1) inputArray.unshift(searchTerm);
     console.log(inputArray);
-    inputArray.forEach(async (filter) => {
+    inputArray.forEach(async (filter, index) => {
       searchResults(filter).then((result) => {
-        console.log(result);
-        console.log(result.length);
-        console.log(block);
-        result.map(async (x) => {
-          const wrapper = document.createElement('div');
-          const newBlock = createCardBlock(x, wrapper);
-          block.append(wrapper);
-          loadBlock(newBlock);
-        });
+        total = totalArray(result);
+        console.log(total);
+        if (index === (inputArray.length - 1)) {
+          createSet(total, block);
+        }
       });
     });
   }
