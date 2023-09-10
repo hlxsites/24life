9bb31d671876d1dbde1a916bb3a4dff591194292
Meet Kumar Jain <meetkjain83@gmail.com>
2023-09-10 13:20:39 +0530

    Without await - calling the entire data

--- blocks/search-results/search-results.js
+++ blocks/search-results/search-results.js
@@ -20,7 +20,7 @@ export default async function decorate(block) {
   // fetch results from json files
   block.innerHTML = '';
   console.log(window.location.origin);
-  const allData = await fetch(`${window.location.origin}/articles.json?sheet=full`);
+  const allData = fetch(`${window.location.origin}/articles.json?sheet=full`);
   console.log(allData);
   const jsonData = await allData.json();
   // for search functionality
