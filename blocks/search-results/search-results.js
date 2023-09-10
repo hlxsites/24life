530669a6d879069e0c12530f769ddac46782cf16
Meet Kumar Jain <meetkjain83@gmail.com>
2023-09-11 03:20:18 +0530

    Coding the load more button aling with its CSS

--- blocks/search-results/search-results.js
+++ blocks/search-results/search-results.js
@@ -69,7 +69,7 @@ function createCards(finalArray, block) {
       await loadBlock(newBlock);
     });
     const counter = block.querySelectorAll('.search-results > .card-wrapper').length / numInitialLoadedArticles;
-    if ((actualLength - (numInitialLoadedArticles * counter)) > 30) {
+    if ((actualLength - (numInitialLoadedArticles * counter)) > numInitialLoadedArticles) {
       createLoadMoreButton(numInitialLoadedArticles, finalArray, actualLength, block);
     }
   }
