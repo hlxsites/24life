92ab767cf860ed85f1565e12c34a6d871182b145
Meet Kumar Jain <meetkjain83@gmail.com>
2023-09-13 17:37:15 +0530

    Revamping - 13th Sept - Bug in the prior Code - moving spinner out from the Grid - having a separate Div

--- blocks/search-results/search-results.js
+++ blocks/search-results/search-results.js
@@ -4,6 +4,65 @@ let newArray = [];
 let total = [];
 let done = true;
 
+export default async function decorate(block) {
+  block.innerHTML = '';
+  const searchTerm = new URLSearchParams(window.location.search).get('q');
+  console.log(searchTerm);
+  block.classList.add('card-container', 'three-columns');
+  if (searchTerm) {
+    const elementHeading = block.parentNode.parentNode.parentNode.querySelector('.section.search-page-heading');
+    const textNode = document.createElement('h1');
+    textNode.textContent = searchTerm;
+    elementHeading.append(textNode);
+    const mainDiv = document.createElement('div');
+    mainDiv.innerHTML = '<div class="results-loading-spinner"><div>';
+    const resultsDiv = document.createElement('div');
+    resultsDiv.classList.add('results-div');
+    mainDiv.append(resultsDiv);
+    block.append(mainDiv);
+    const tokenizedSearchWords = searchTerm.split(' ');
+    if (tokenizedSearchWords.length > 1) tokenizedSearchWords.unshift(searchTerm);
+    loadResults(tokenizedSearchWords, resultsDiv);
+  }
+}
+
+function loadResults(tokenizedSearchWords, resultsDiv) {
+  tokenizedSearchWords.forEach(async (filter, index) => {
+    searchResults(filter).then((result) => {
+      total = totalArray(result);
+      console.log(total);
+      if (index === (tokenizedSearchWords.length - 1)) {
+        if (total.length === 0) {
+          document.querySelector('main .results-loading-spinner').style.display = 'none';
+          const sorryDiv = document.createElement('div');
+          const sorryPara = document.createElement('p');
+          sorryDiv.append(sorryPara);
+          sorryPara.textContent = 'Sorry, no results were found, search again ?';
+          resultsDiv.append(sorryDiv);
+          sorryDiv.classList.add('no-results');
+          const searchFormDiv = document.createElement('div');
+          sorryDiv.append(searchFormDiv);
+          searchFormDiv.innerHTML = `
+           <div class="search-container">
+            <div class="search-wrapper">
+             <div class='search-form'>
+              <form action='/search' method='get'>
+                <input type='search' name='q' class='search-input' placeholder="TYPE HERE"/>
+              </form>
+             </div>
+            </div>
+           </div>
+         `;
+          document.querySelector('.block.search-results.card-container.three-columns .results-div').style.display = 'block';
+        } else {
+          console.log(tokenizedSearchWords);
+          createSet(total, resultsDiv);
+        }
+      }
+    });
+  });
+}
+
 export async function searchResults(params) {
   // fetch results from json files
   const allData = await fetch(`${window.location.origin}/articles.json?sheet=full`);
@@ -27,12 +86,12 @@ function totalArray(arrayChunk) {
   return newArray;
 }
 
-function displayNextEntries(iterator, numInitialLoadedArticles, loadMoreContainer, block) {
-  console.log(done);
-  if (done) { block.querySelector('.results-loading-spinner').remove(); done = false; }
+function displayNextEntries(iterator, numInitialLoadedArticles, loadMoreContainer, resultsDiv) {
+  console.log(resultsDiv);
+  if (done) { resultsDiv.parentNode.querySelector('.results-loading-spinner').remove(); done = false; }
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i <= numInitialLoadedArticles; i++) {
-    if (i === numInitialLoadedArticles) { block.after(loadMoreContainer); }
+    if (i === numInitialLoadedArticles) { resultsDiv.after(loadMoreContainer); }
     const next = iterator.next();
     if (next.done) {
       loadMoreContainer.remove();
@@ -41,24 +100,24 @@ function displayNextEntries(iterator, numInitialLoadedArticles, loadMoreContaine
     if (!searchItem) break;
     const wrapper = document.createElement('div');
     createCardBlock(searchItem, wrapper);
-    block.append(wrapper);
+    resultsDiv.append(wrapper);
   }
 }
 
-function createCards(finalArray, block) {
+function createCards(finalArray, resultsDiv) {
   console.log(finalArray);
   const numInitialLoadedArticles = 23;
   const iterator = finalArray.values();
   const loadMoreContainer = document.createElement('div');
   loadMoreContainer.classList.add('article-load-more-container');
   loadMoreContainer.innerHTML = '<button class="article-list-load-more-button">Load more</button>';
-  displayNextEntries(iterator, numInitialLoadedArticles, loadMoreContainer, block);
+  displayNextEntries(iterator, numInitialLoadedArticles, loadMoreContainer, resultsDiv);
   loadMoreContainer.addEventListener('click', () => {
-    displayNextEntries(iterator, numInitialLoadedArticles, loadMoreContainer, block);
+    displayNextEntries(iterator, numInitialLoadedArticles, loadMoreContainer, resultsDiv);
   });
 }
 
-function createSet(sumArray, block) {
+function createSet(sumArray, resultsDiv) {
   const uniquePath = new Set();
   const finalArray = sumArray.filter((element) => {
     const isDuplicate = uniquePath.has(element.path);
@@ -68,61 +127,61 @@ function createSet(sumArray, block) {
     }
     return false;
   });
-  createCards(finalArray, block);
+  createCards(finalArray, resultsDiv);
 }
 
-function calculate(inputArray, block) {
-  inputArray.forEach(async (filter, index) => {
-    searchResults(filter).then((result) => {
-      total = totalArray(result);
-      console.log(total);
-      if (index === (inputArray.length - 1)) {
-        if (total.length === 0) {
-          document.querySelector('main .results-loading-spinner').style.display = 'none';
-          const sorryDiv = document.createElement('div');
-          const sorryPara = document.createElement('p');
-          sorryDiv.append(sorryPara);
-          sorryPara.textContent = 'Sorry, no results were found, search again ?';
-          block.append(sorryDiv);
-          sorryDiv.classList.add('no-results');
-          const searchFormDiv = document.createElement('div');
-          sorryDiv.append(searchFormDiv);
-          searchFormDiv.innerHTML = `
-           <div class="search-container">
-            <div class="search-wrapper">
-             <div class='search-form'>
-              <form action='/search' method='get'>
-                <input type='search' name='q' class='search-input' placeholder="TYPE HERE"/>
-              </form>
-             </div>
-            </div>
-           </div>
-         `;
-          document.querySelector('.block.search-results.card-container.three-columns').style.display = 'block';
-        } else {
-          console.log(inputArray);
-          createSet(total, block);
-        }
-      }
-    });
-  });
-}
+// function calculate(inputArray, block) {
+//   inputArray.forEach(async (filter, index) => {
+//     searchResults(filter).then((result) => {
+//       total = totalArray(result);
+//       console.log(total);
+//       if (index === (inputArray.length - 1)) {
+//         if (total.length === 0) {
+//           document.querySelector('main .results-loading-spinner').style.display = 'none';
+//           const sorryDiv = document.createElement('div');
+//           const sorryPara = document.createElement('p');
+//           sorryDiv.append(sorryPara);
+//           sorryPara.textContent = 'Sorry, no results were found, search again ?';
+//           block.append(sorryDiv);
+//           sorryDiv.classList.add('no-results');
+//           const searchFormDiv = document.createElement('div');
+//           sorryDiv.append(searchFormDiv);
+//           searchFormDiv.innerHTML = `
+//            <div class="search-container">
+//             <div class="search-wrapper">
+//              <div class='search-form'>
+//               <form action='/search' method='get'>
+//                 <input type='search' name='q' class='search-input' placeholder="TYPE HERE"/>
+//               </form>
+//              </div>
+//             </div>
+//            </div>
+//          `;
+//           document.querySelector('.block.search-results.card-container.three-columns').style.display = 'block';
+//         } else {
+//           console.log(inputArray);
+//           createSet(total, block);
+//         }
+//       }
+//     });
+//   });
+// }
 
-export default async function decorate(block) {
-  block.innerHTML = '';
-  const searchTerm = new URLSearchParams(window.location.search).get('q');
-  console.log(searchTerm);
-  block.classList.add('card-container', 'three-columns');
-  if (searchTerm) {
-    const elementHeading = block.parentNode.parentNode.parentNode.querySelector('.section.search-page-heading');
-    const textNode = document.createElement('h1');
-    textNode.textContent = searchTerm;
-    elementHeading.append(textNode);
-    const spinnerDiv = document.createElement('div');
-    spinnerDiv.classList.add('results-loading-spinner');
-    block.append(spinnerDiv);
-    const inputArray = searchTerm.split(' ');
-    if (inputArray.length > 1) inputArray.unshift(searchTerm);
-    calculate(inputArray, block);
-  }
-}
+// export default async function decorate(block) {
+//   block.innerHTML = '';
+//   const searchTerm = new URLSearchParams(window.location.search).get('q');
+//   console.log(searchTerm);
+//   block.classList.add('card-container', 'three-columns');
+//   if (searchTerm) {
+//     const elementHeading = block.parentNode.parentNode.parentNode.querySelector('.section.search-page-heading');
+//     const textNode = document.createElement('h1');
+//     textNode.textContent = searchTerm;
+//     elementHeading.append(textNode);
+//     const spinnerDiv = document.createElement('div');
+//     spinnerDiv.classList.add('results-loading-spinner');
+//     block.append(spinnerDiv);
+//     const inputArray = searchTerm.split(' ');
+//     if (inputArray.length > 1) inputArray.unshift(searchTerm);
+//     calculate(inputArray, block);
+//   }
+// }
