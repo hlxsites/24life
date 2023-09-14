c29c51cec66e0eb8dbf1e6bd0055c03067e44f76
Aravindo Wingeier <wingeier@adobe.com>
2023-09-14 10:34:05 -0700

    Add UI simple tests

---
+++ .github/visual-tests/run-locally.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#export DOMAIN_MAIN="main--vg-macktrucks-com--hlxsites.hlx.page"
+#export DOMAIN_BRANCH="update-from-boilerplate--vg-macktrucks-com--hlxsites.hlx.page"
+
+export DOMAIN_MAIN="main--24life--hlxsites.hlx.live"
+export DOMAIN_BRANCH="author-bug--24life--hlxsites.hlx.live"
+
+export TEST_PATHS="/ /block-library/blocks/columns /block-library/blocks/card /block-library/blocks/article-hero /block-library/blocks/article-author /block-library/blocks/article-list /block-library/blocks/article-carousel /block-library/blocks/magazine-hero /block-library/blocks/quote /block-library/blocks/youtube /block-library/blocks/spotify /block-library/blocks/carousel /block-library/blocks/article-hero-video /block-library/blocks/video /block-library/blocks/author-list /block-library/blocks/float-images"
+
+# we ignore the exit code of the test command because we want to continue
+npx playwright test
+set -e
+
+cat test-results/visual-diff.md
