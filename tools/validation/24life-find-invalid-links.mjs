13a5fc818dbf1e70ff3dd5826b185f8ad37370ec
Aravindo Wingeier <wingeier@adobe.com>
2023-09-21 15:14:38 -0700

    24life-find-invalid-links.mjs: refactor 2

--- tools/validation/24life-find-invalid-links.mjs
+++ tools/validation/24life-find-invalid-links.mjs
@@ -14,131 +14,132 @@ const dir = `./24life`;
 requireNode20();
 
 const redirects = await loadRedirects();
-const allIssues = {}
 
-let htmlFiles = (await readdir(dir, {recursive: true}))
-    .filter((file) => file.endsWith(".html"))
-    .map((file) => dir + "/" + file)
-
-await Promise.all(htmlFiles.map(async (file) => validateFile(file)));
-
-// print result
-if (!Object.keys(allIssues).length) {
-    console.log("no issues found");
-    process.exit(0);
+/**
+ * @param url
+ * @returns {string|undefined} return the url to change to, or null if no change is required
+ */
+function linkMapping(url) {
+    if (url.startsWith("http") && url.includes("24life.com/")) {
+        let redirectFullUrl = getRedirectFullUrl(url);
+        if (!redirectFullUrl && !url.endsWith("/")) {
+            // assume the same pathname also exists on the new site
+            redirectFullUrl = "https://main--24life--hlxsites.hlx.page" + new URL(url).pathname
+        }
+        return redirectFullUrl;
+    }
 }
 
-Object.keys(allIssues).forEach((key) => {
-    console.log("https://main--24life--hlxsites.hlx.page" + key);
-    console.log(allIssues[key]);
-});
-console.log("total issues:", Object.keys(allIssues).length);
-Object.values(allIssues).flatMap((issues) => issues).forEach((issue) => {
-    console.log(issue.link, "→", issue.redirect);
-});
+function validateLink(url) {
+    if (url.includes("twentyfourlife.wpenginepowered")) {
+        return "url includes twentyfourlife.wpenginepowered"
+    }
+    if (url.includes(".hlx.")) {
+        if (url.matches(new RegExp("/(focus|fitness|fuel|recover)[/][0-9]{4}[/].+"))) {
+            // correct links
+        } else {
+            if (getRedirectFullUrl(url)) {
+                // if there is a redirect, we will rely on that. We are not changing all the links at this moment.
+            } else {
+                return "no redirect found for hlx link";
+            }
+        }
+    }
+}
 
+async function main() {
+    const {allLinkChanges, allLinkWarnings} = await findLinksToChange();
 
-// ask user for confirmation
-const answer = await new Promise(resolve => {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
+    if (!Object.keys(allLinkChanges).length && !Object.keys(allLinkWarnings).length) {
+        console.log("no issues found");
+        process.exit(0);
+    }
+
+    for (const key of Object.keys(allLinkChanges)) {
+        console.log("https://main--24life--hlxsites.hlx.page" + key);
+        console.log(allLinkChanges[key]);
+    }
+    console.log("----")
+    console.log("Warnings:")
+
+    for (const key of Object.keys(allLinkWarnings)) {
+        console.log("https://main--24life--hlxsites.hlx.page" + key);
+        console.log(allLinkWarnings[key]);
+    }
+
+    console.log("----")
+    console.log("all link changes:")
+    Object.values(allLinkChanges).flatMap((issues) => issues).forEach((issue) => {
+        console.log(issue.link, "→", issue.newurl);
     });
-    rl.question(`Do you want to rewrite the urls for ${Object.keys(allIssues).length} issues? (y/N)`, resolve)
-})
-if (answer !== "y") {
-    console.log("aborting")
+
+    // rewrite docx files
+    await userConfirmToContinue(`Do you want to change ${Object.keys(allLinkChanges).length} files? (y/N)`);
+    await fs.mkdir(outputDir, {recursive: true});
+    for (const file of Object.keys(allLinkChanges)) {
+        await rewriteDocx(file, outputDir, allLinkChanges[file]);
+    }
+    console.log("rewriting changed files in ", outputDir)
+
+    console.log("urls: ", Object.keys(allLinkChanges).map((key) => "https://main--24life--hlxsites.hlx.page" + key).join("\n"));
+    console.log("files: ", Object.keys(allLinkChanges).map((key) => "./24life" + key + ".html").join(" "));
+
     process.exit(0);
 }
 
-// create output directory
-await fs.mkdir(outputDir, {recursive: true});
-
-
-// rewrite urls
-for (const file of Object.keys(allIssues)) {
-    await rewriteDocx(file);
-}
-console.log("rewriting changed files in ", outputDir)
-
-console.log("urls: ", Object.keys(allIssues).map((key) => "https://main--24life--hlxsites.hlx.page" + key).join("\n"));
-console.log("files: ", Object.keys(allIssues).map((key) => "./24life" + key + ".html").join(" "));
-
-process.exit(0);
-
-
-async function rewriteDocx(file) {
+async function rewriteDocx(file, outputDir, changes) {
     const filePath = path.join(mountDirectory, file + ".docx");
     const targetFilePath = path.join(outputDir, file + ".docx");
 
-    // copy file to target, so it can be modified multiple times without overwriting the original
+    // copy file to target, so it can be modified multiple times
     const parentDir = targetFilePath.substring(0, targetFilePath.lastIndexOf("/"));
     await fs.mkdir(parentDir, {recursive: true});
     await fs.copyFile(filePath, targetFilePath);
-    let anyChanges = false;
-    for (const link of allIssues[file]) {
-        if (link.redirect) {
+    for (const link of changes) {
+        if (link.newurl) {
             await fs.copyFile(targetFilePath, `${targetFilePath}.tmp.docx`);
-            const command = `docxtools "${targetFilePath}.tmp.docx" replace-links "${link.link}" "${link.redirect}" "${targetFilePath}"`;
-            console.debug(command)
+            const command = `docxtools "${targetFilePath}.tmp.docx" replace-links "${link.link}" "${link.newurl}" "${targetFilePath}"`;
+            // console.debug(command)
             console.log(await execShellCommand(command))
             await fs.unlink(`${targetFilePath}.tmp.docx`);
-            anyChanges = true;
         } else {
-            console.log("--")
-            console.log(file)
-            console.log("skipping", link.link, "because no redirect found")
+            throw new Error("no newurl found")
         }
     }
-    if (!anyChanges) {
-        await fs.unlink(targetFilePath);
-    }
-}
-
-function hasRedirect(url) {
-    return !!getRedirect(url);
-}
-
-function getRedirect(url) {
-    return redirects[new URL(url).pathname] || redirects[new URL(url).pathname + "/"];
 }
 
 function getRedirectFullUrl(url) {
-    const result = getRedirect(url);
-    if (result) {
-        return "https://main--24life--hlxsites.hlx.page" + result;
+    const redirect = redirects[new URL(url).pathname] || redirects[new URL(url).pathname + "/"];
+    if (redirect) {
+        return "https://main--24life--hlxsites.hlx.page" + redirect;
     }
 }
 
-async function validateFile(filePath) {
-    const issues = []
-
+async function getAllLinks(filePath) {
     const content = await fs.readFile(filePath, 'utf-8');
     const dom = new JSDOM(content);
-    const links = dom.window.document.querySelectorAll('a[href]');
+    return dom.window.document.querySelectorAll('a[href]');
+}
+
+async function validateLinks(links, key, allLinkChanges, allLinkWarnings) {
+    const linkChanges = []
+    const linkWarnings = []
     for (let link of links) {
-        // console.log(link.href, link.textContent)
-        if (link.href.startsWith("http") && link.href.includes("24life.com/")) {
-            const redirectFullUrl = getRedirectFullUrl(link.href);
-            if (!redirectFullUrl) {
-                // assume the same pathname still exists
-                return "https://main--24life--hlxsites.hlx.page" + new URL(link.href).pathname
-            }
-            issues.push({link: link.href, text: link.textContent, redirect: redirectFullUrl})
+        const newurl = linkMapping(link.href);
+        if (newurl) {
+            linkChanges.push({link: link.href, text: link.textContent, newurl});
         }
-        if (link.href.includes("twentyfourlife.wpenginepowered")) {
-            issues.push({link: link.href, text: link.textContent})
-        }
-        if (link.href.includes(".hlx.")) {
-            if (link.href.matches(new RegExp("/(focus|fitness|fuel|recover)[/][0-9]{4}[/].+"))) {
-                // correct links
-            } else if (!hasRedirect(link.href)) {
-                issues.push({link: link.href, text: link.textContent})
-            }
+        const warning = validateLink(link.href);
+        if (warning) {
+            linkWarnings.push({link: link.href, text: link.textContent, warning});
         }
     }
-    if (issues.length) {
-        allIssues[filePath.replace("./24life/", "/").replace(".html", "")] = issues;
+
+    if (linkChanges.length) {
+        allLinkChanges[key] = linkChanges;
+    }
+    if (linkWarnings.length) {
+        allLinkWarnings[key] = linkWarnings;
     }
 }
 
@@ -177,3 +178,39 @@ function requireNode20() {
         process.exit(1)
     }
 }
+
+
+async function userConfirmToContinue(question) {
+    const answer = await new Promise(resolve => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question(question, resolve)
+    })
+    if (answer !== "y") {
+        console.log("aborting")
+        process.exit(0);
+    }
+}
+
+
+async function findLinksToChange() {
+    const allLinkChanges = {}
+    const allLinkWarnings = {}
+
+    let htmlFiles = (await readdir(dir, {recursive: true}))
+        .filter((file) => file.endsWith(".html"))
+        .map((file) => dir + "/" + file)
+
+    await Promise.all(htmlFiles.map(async (file) => {
+        const key = file.replace("./24life/", "/").replace(".html", "");
+        const links = await getAllLinks(file);
+        return validateLinks(links, key, allLinkChanges, allLinkWarnings);
+    }));
+
+    return {allLinkChanges, allLinkWarnings};
+}
+
+
+await main();
