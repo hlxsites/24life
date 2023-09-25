7058a796544f1f8b8db31a2a4de72a84e579c26e
Aravindo Wingeier <wingeier@adobe.com>
2023-09-25 11:23:50 -0700

    /magazine: improve LHS

--- blocks/magazine-list/magazine-list.js
+++ blocks/magazine-list/magazine-list.js
@@ -1,4 +1,3 @@
-import ffetch from '../../scripts/ffetch.js';
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default async function decorate(block) {
@@ -8,37 +7,42 @@ export default async function decorate(block) {
   otherIssues.className = 'other-issues';
   block.append(currentIssue);
   block.append(otherIssues);
-  await fetchMagazines(currentIssue, otherIssues);
+
+  const magazines = await fetchMagazines();
+  renderMagazines(magazines, currentIssue, otherIssues);
 }
 
-async function fetchMagazines(currentIssue, otherIssues) {
-  const magazines = await ffetch('/magazines.json').all();
+async function fetchMagazines() {
+  const response = await fetch('/magazines.json');
+  const json = await response.json();
+  return json.data;
+}
 
-  await Promise.all(magazines
-    .map(async (issue) => {
-      const issueDiv = document.createElement('div');
-      issueDiv.className = 'issue';
+function renderMagazines(magazines, currentIssue, otherIssues) {
+  magazines.forEach((issue, index) => {
+    const issueDiv = document.createElement('div');
+    issueDiv.className = 'issue';
 
-      const issuePath = document.createElement('a');
-      issuePath.href = issue.path;
+    const issuePath = document.createElement('a');
+    issuePath.href = issue.path;
 
-      const issueImage = createOptimizedPicture(issue.image, issue.title, true);
-      issueImage.className = 'issue-image';
+    const issueImage = createOptimizedPicture(issue.image, issue.title, true);
+    issueImage.className = 'issue-image';
 
-      const titleDiv = document.createElement('div');
-      titleDiv.className = 'issue-title';
-      titleDiv.textContent = issue.title;
-      const titleLink = issuePath.cloneNode();
-      titleLink.append(titleDiv);
+    const titleDiv = document.createElement('div');
+    titleDiv.className = 'issue-title';
+    titleDiv.textContent = issue.title;
+    const titleLink = issuePath.cloneNode();
+    titleLink.append(titleDiv);
 
-      issuePath.append(issueImage);
-      issueDiv.append(issuePath);
-      issueDiv.append(titleLink);
+    issuePath.append(issueImage);
+    issueDiv.append(issuePath);
+    issueDiv.append(titleLink);
 
-      if (currentIssue.children.length === 0) {
-        currentIssue.append(issueDiv);
-      } else {
-        otherIssues.append(issueDiv);
-      }
-    }));
+    if (index === 0) {
+      currentIssue.append(issueDiv);
+    } else {
+      otherIssues.append(issueDiv);
+    }
+  });
 }
