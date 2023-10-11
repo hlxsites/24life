8723f9b89c03b50d54c66a8fe5ab079cb9f566dc
Meet Kumar Jain <meetkjain83@gmail.com>
2023-10-12 02:23:58 +0530

    Updating the placement of texts and Enter button

--- 24life/blocks/newsletter-form/newsletter-form.js
+++ 24life/blocks/newsletter-form/newsletter-form.js
@@ -3,13 +3,13 @@ export default function decorate(block) {
   block.innerHTML = `<form id="subForm" style="line-height: 1.5em;" action="http://newsletter.culturefoundry.com/t/r/s/hruuutt/" method="post">
   <div id="email-container"><label><strong>Email</strong></label><input id="fieldEmail" style="width: 300px; font-size: 14px;" name="cm-hruuutt-hruuutt" required="" type="text" /></div>
   <div class="td-pb-row margin">
-  <p><label><strong>Which topics are you most interested in reading about? (Choose all that apply.)</strong></label><br />
+  <p><label><strong>Which topics are you most interested in reading about? (Choose all that apply.)</strong></label><br /><br />
   <input id="fieldvukjyyd-0" name="cm-fo-vukjyyd" type="checkbox" value="7491224" /> <label for="fieldvukjyyd-0">Mindset (meditation, motivation, leadership, time management)</label><br />
   <input id="fieldvukjyyd-1" name="cm-fo-vukjyyd" type="checkbox" value="7491225" /> <label for="fieldvukjyyd-1">Movement (workouts, cardio, technique tips, strength tips, training plans)</label><br />
   <input id="fieldvukjyyd-2" name="cm-fo-vukjyyd" type="checkbox" value="7491226" /> <label for="fieldvukjyyd-2">Nourishment (recipes, nutrition insights, meal-time rituals)</label><br />
   <input id="fieldvukjyyd-3" name="cm-fo-vukjyyd" type="checkbox" value="7491227" /> <label for="fieldvukjyyd-3">Regeneration (rest and recovery, stress management, rejuvenation)</label><br />
   <input id="fieldvukjyyd-4" name="cm-fo-vukjyyd" type="checkbox" value="7491228" /> <label for="fieldvukjyyd-4">Lifestyle (fashion, travel, books, movies, life hacks)</label></p>
-  <p><label><strong>What is your number one health or fitness goal?</strong></label><br />
+  <p><label><strong>What is your number one health or fitness goal?</strong></label><br /><br />
   <input id="fieldvukjyjh-0" name="cm-fo-vukjyjh" type="checkbox" value="7491236" /> <label for="fieldvukjyjh-0">Get more sleep</label><br />
   <input id="fieldvukjyjh-1" name="cm-fo-vukjyjh" type="checkbox" value="7491237" /> <label for="fieldvukjyjh-1">Reduce stress</label><br />
   <input id="fieldvukjyjh-2" name="cm-fo-vukjyjh" type="checkbox" value="7491238" /> <label for="fieldvukjyjh-2">Get healthier</label><br />
@@ -19,7 +19,7 @@ export default function decorate(block) {
   <input id="fieldvukjyjh-6" name="cm-fo-vukjyjh" type="checkbox" value="7514118" /> <label for="fieldvukjyjh-6">Feel more energetic</label><br />
   <input id="fieldvukjyjh-7" name="cm-fo-vukjyjh" type="checkbox" value="7514119" /> <label for="fieldvukjyjh-7">Relieve pain</label><br />
   <input id="fieldvukjyjh-8" name="cm-fo-vukjyjh" type="checkbox" value="7514120" /> <label for="fieldvukjyjh-8">Perform better</label></p>
-  <p><label><strong>What are your favorite forms of fitness?</strong></label><br />
+  <p><label><strong>What are your favorite forms of fitness?</strong></label><br /><br />
   <input id="fieldvukjyjd-0" name="cm-fo-vukjyjd" type="checkbox" value="7514116" /> <label for="fieldvukjyjd-0">Yoga</label><br />
   <input id="fieldvukjyjd-1" name="cm-fo-vukjyjd" type="checkbox" value="7491229" /> <label for="fieldvukjyjd-1">Running</label><br />
   <input id="fieldvukjyjd-2" name="cm-fo-vukjyjd" type="checkbox" value="7491230" /> <label for="fieldvukjyjd-2">Biking</label><br />
@@ -28,7 +28,7 @@ export default function decorate(block) {
   <input id="fieldvukjyjd-5" name="cm-fo-vukjyjd" type="checkbox" value="7491233" /> <label for="fieldvukjyjd-5">Team sports</label><br />
   <input id="fieldvukjyjd-6" name="cm-fo-vukjyjd" type="checkbox" value="7491234" /> <label for="fieldvukjyjd-6">Outdoor activities</label><br />
   <input id="fieldvukjyjd-7" name="cm-fo-vukjyjd" type="checkbox" value="7491235" /> <label for="fieldvukjyjd-7">Other</label></p>
-  <p><label><strong>We'd love to know if you're:</strong></label><br />
+  <p><label><strong>We'd love to know if you're:</strong></label><br /><br />
   <input id="fieldvukjyjk-0" name="cm-fo-vukjyjk" type="checkbox" value="7491241" /> <label for="fieldvukjyjk-0">Male</label><br />
   <input id="fieldvukjyjk-1" name="cm-fo-vukjyjk" type="checkbox" value="7491242" /> <label for="fieldvukjyjk-1">Female</label><br />
   <input id="fieldvukjyjk-2" name="cm-fo-vukjyjk" type="checkbox" value="7491243" /> <label for="fieldvukjyjk-2">18-24</label><br />
@@ -37,7 +37,7 @@ export default function decorate(block) {
   <input id="fieldvukjyjk-5" name="cm-fo-vukjyjk" type="checkbox" value="7491246" /> <label for="fieldvukjyjk-5">45-54</label><br />
   <input id="fieldvukjyjk-6" name="cm-fo-vukjyjk" type="checkbox" value="7491247" /> <label for="fieldvukjyjk-6">55-64</label><br />
   <input id="fieldvukjyjk-7" name="cm-fo-vukjyjk" type="checkbox" value="7491248" /> <label for="fieldvukjyjk-7">65+</label></p>
-  <p><label for="fieldeztrdi"><strong>Finally, what is your zip code?</strong><br />
+  <p><label for="fieldeztrdi"><strong>Finally, what is your zip code?</strong><br /><br />
   <input id="fieldeztrdi" style="width: 125px;" name="cm-f-eztrdi" size="5" type="text" /></label></p>
   </div>
   <div class="td-pb-row margin"><button class="promo-btn" style="padding: 10px 70px;" type="submit">Enter</button></div>
