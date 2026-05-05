ef7a834bb50996f40aa8aec906c60715b30c8c24
Jose Martinez <jmartinez@24hourfit.com>
2026-05-05 16:06:17 -0600

    custom margin adjustment

--- 24life/scripts/ada-bug-integration.js
+++ 24life/scripts/ada-bug-integration.js
@@ -1,6 +1,6 @@
 import { loadScript } from './lib-franklin.js';
 
-await loadScript('https://a42cdn.usablenet.com/a42/24hourfitness/default/prod/cs-start?margin=0', {
+await loadScript('https://a42cdn.usablenet.com/a42/24hourfitness/default/prod/cs-start', {
   id: 'usntA42start',
   async: true,
   'data-rapid': 'true',
